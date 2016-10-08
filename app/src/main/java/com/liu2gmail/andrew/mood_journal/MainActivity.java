8ba3f30a58752dd62274f4281f848f8fa8839ec3
Andrew Liu <andrew.liu2@gmail.com>
2016-10-08 03:10:29 -0400

    Initial commit for Android files.

---
+++ app/src/main/java/com/liu2gmail/andrew/mood_journal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.liu2gmail.andrew.mood_journal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
